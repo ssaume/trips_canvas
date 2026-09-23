f35c8ebfd42b97064e7ab84f6c5dc984b0ebe53e
ssaume <ssaume@gmail.com>
2026-09-23 21:37:51 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-window.TRIP_CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbyouCtqp0QNVxDLCd43JrQPXbnC8ydUaKsoJzEjvWLai_NDKIemeJL9F2HDe3tVOlD3sw/exec",
-  NOMINATIM_URL: "https://nominatim.openstreetmap.org/search",
-  DEFAULT_CENTER: { lat: 23.6978, lng: 120.9605 },
-  DEFAULT_ZOOM: 7
-};
